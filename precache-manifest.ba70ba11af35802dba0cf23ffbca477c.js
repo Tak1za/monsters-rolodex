3898a2a16bad94d836e9ff998c89cf931060d494
Varun Gupta <varun.gupta3@honeywell.com>
2020-03-27 19:58:51 +0530

    Updates

---
+++ precache-manifest.ba70ba11af35802dba0cf23ffbca477c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b951c69a315697061d5d99003e39dfb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "950dade10bae09e20d3c",
+    "url": "/monsters-rolodex/static/css/main.210f12aa.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "950dade10bae09e20d3c",
+    "url": "/monsters-rolodex/static/js/main.e54bb59e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
