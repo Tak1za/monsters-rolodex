55443a899fde7af10f83236bbe7e1a1f34b634a0
Varun Gupta <varun.gupta3@honeywell.com>
2020-03-27 18:48:31 +0530

    Created the monster list and card component

---
+++ src/components/card/card-component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './card.style.css';
+
+export const Card = props => (
+	<div className="card-container">
+		<img
+			alt="monster"
+			src={`https://robohash.org/${props.monster.id}?set=set2`}
+			height="180px"
+			width="180px"
+		/>
+		<h2>{props.monster.name}</h2>
+		<p> {props.monster.email}</p>
+	</div>
+);
